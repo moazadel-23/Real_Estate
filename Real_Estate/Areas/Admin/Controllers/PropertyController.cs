a06e6033c6a0e6b22276094fc7659d7965acf77f
moazadel-23 <moazaboelfadle@gmail.com>
2026-02-06 01:12:16 +0200

    add property and mange property

--- Real_Estate/Areas/Admin/Controllers/PropertyController.cs
+++ Real_Estate/Areas/Admin/Controllers/PropertyController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Real_Estate.Repository;
+using System.Threading;
+using System.Threading.Tasks;
 
 
 namespace Real_Estate.Areas.Admin.Controllers
@@ -18,6 +21,69 @@ namespace Real_Estate.Areas.Admin.Controllers
         {
             var properties = await _propertyRepository.GetAllAsync();
             return View(properties.AsEnumerable());
+        }
+        public async Task<IActionResult> board()
+        {
+            var properties = await _propertyRepository.GetAllAsync();
+            return View(properties.AsEnumerable());
+        }
+        [HttpGet]
+        public async Task<IActionResult> manage()
+        {
+            var properties = await _propertyRepository.GetAllAsync();
+            return View(properties.AsEnumerable());
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create(CancellationToken cancellationToken)
+        {
+            var properties =  await _propertyRepository.GetAllAsync(cancellationToken : cancellationToken);
+            return View(new Models.Property());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Models.Property property, IFormFile Img, IFormFileCollection SubImgFiles, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(property);
+            }
+
+            var uploadPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "PropertyImages");
+            Directory.CreateDirectory(uploadPath);
+
+            if (Img != null && Img.Length > 0)
+            {
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img");
+                Directory.CreateDirectory(folder);
+                var fileName = Guid.NewGuid() + Path.GetExtension(Img.FileName);
+                var filePath = Path.Combine(folder, fileName);
+                using var stream = System.IO.File.Create(filePath);
+                await Img.CopyToAsync(stream);
+                property.MainImg = fileName;
+            }
+
+
+            if (SubImgFiles != null && SubImgFiles.Count > 0)
+            {
+                property.MainImg = "";
+                foreach (var file in SubImgFiles)
+                {
+                    var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img");
+                    Directory.CreateDirectory(folder);
+                    var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    var filePath = Path.Combine(folder, fileName);
+                    using var stream = System.IO.File.Create(filePath);
+                    await file.CopyToAsync(stream);
+                    property.MainImg += fileName + ";";
+                }
+            }
+
+            await _propertyRepository.AddAsync(property, cancellationToken);
+            await _propertyRepository.CommitChange(cancellationToken);
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Real_Estate/Models/ViewModel/PropertyLocVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Real_Estate.Models.ViewModel
+{
+    public class PropertyLocVM
+    {
+        public Property Property { get; set; } = new Property();
+        public Location  Location { get; set; } = new Location();
+    }
+}
