a18c01c00d5375bcd903ed0234234bf98cec215e
Ali_mohamed <onethecrazy034@gmail.com>
2026-02-05 02:59:14 +0200

    ALI_CRUD

--- Real_Estate/DataAccess/ApplicationDbContext.cs
+++ Real_Estate/DataAccess/ApplicationDbContext.cs
@@ -1,22 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Real_Estate.Models;
 
 namespace Real_Estate.DataAccess
 {
     public class ApplicationDbContext : DbContext
     {
-        DbSet<Property> Properties { get; set; }
-        DbSet<PropertyImage> PropertyImages { get; set; }
-        DbSet<Location> Locations { get; set; }
-        DbSet<Favorite> favorites { get; set; }
-        DbSet<User> users { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
 
-
-
-        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog= Real_Estate_G10;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30");
-
-        }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<PropertyImage> PropertyImages { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
