254ce65588afc38b358529f80571f784665285d8
walidmohamedfarag <walid492003@gmail.com>
2026-02-06 06:48:08 +0200

    DI Services : add file AddServices inside DI_Service folder to add service when use the dependency injection

---
+++ Real_Estate/DI_Service/AddService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Real_Estate.DI_Service
+{
+    public class AddService
+    {
+        public void AddScopedServices(IServiceCollection services)
+        {
+            services.AddIdentity<User, IdentityRole>(option =>
+            {
+                option.Password.RequireNonAlphanumeric = false;
+                option.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+        }
+    }
+}
