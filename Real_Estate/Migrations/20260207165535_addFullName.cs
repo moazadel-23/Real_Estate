36d79b6fc1642fbd965cacbac1e256dd6d807d35
moazadel-23 <moazaboelfadle@gmail.com>
2026-02-07 18:56:29 +0200

    addFullname

---
+++ Real_Estate/Migrations/20260207165535_addFullName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Real_Estate.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFullName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "users",
+                newName: "FullName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "users",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
