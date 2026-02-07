e4da982630ea747e897a0317828189b6f5f10203
walidmohamedfarag <walid492003@gmail.com>
2026-02-07 18:41:07 +0200

    user model : edite prop name to full name

--- Real_Estate/Models/User.cs
+++ Real_Estate/Models/User.cs
@@ -4,8 +4,7 @@ namespace Real_Estate.Models
 {
     public class User : IdentityUser
     {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
     }
