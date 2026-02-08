9ec24fe623f3940a0983a8be7087adc974a412ec
moazadel-23 <moazaboelfadle@gmail.com>
2026-02-08 21:00:50 +0200

    nn

--- Real_Estate/Models/User.cs
+++ Real_Estate/Models/User.cs
@@ -7,8 +7,8 @@ namespace Real_Estate.Models
         public string FullName { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
-        //public string LastName { get; internal set; }
-        //public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string FirstName { get; internal set; }
     }
 
 }
