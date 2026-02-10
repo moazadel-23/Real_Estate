4eda4d3205e632da8be44879b81b8aea72a735fe
moazadel-23 <moazaboelfadle@gmail.com>
2026-02-11 00:34:24 +0200

    Customer Area

---
+++ Real_Estate/Areas/Customer/Controllers/FavoriteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Real_Estate.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class FavoriteController : Controller
+    {
+        [HttpGet]
+        public IActionResult favPage()
+        {
+            return View();
+        }
+    }
+}
